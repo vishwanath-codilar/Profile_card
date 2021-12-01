6bf78915273c6bfc7c0894512d3bf3b718b00d1e
vishwanath <vishwanath.t@codilar.com>
2021-12-01 18:06:42 +0530

    profile_scss changed

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import "../scss/app.scss";
-// import '../contentloader/loader'
+
 import Loader from '../contentloader/loader';
 
 export default function Profile({ state }) {
